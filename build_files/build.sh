355a38807cfcc6abcfd44444316a59607733690f
goek-han <goek-han@users.noreply.github.com>
2026-04-02 10:07:45 +0200

    Add Development Tools group installation to build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -26,5 +26,7 @@ dnf5 install -y wireshark
 # install nix
 # from https://fedoraproject.org/wiki/Changes/Nix_package_tool#How_To_Test
 mkdir -p /nix
-dnf5 install -y nix nix-daemon
-systemctl enable nix-daemon
+
+# install additional dev tools
+dnf install -y @development-tools
+dnf5 install -y python3-wheel python3-devel python3-netifaces
